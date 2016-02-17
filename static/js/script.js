3f706452e6a89e35a176f0e8d139f022b052220c
jarnovnico <j.verhoogt@gmail.com>
2016-02-17 15:17:52 +0100

    asdsada

---
+++ static/js/script.js
@@ -0,0 +1,26 @@
+// Your use of the YouTube API must comply with the Terms of Service:
+// https://developers.google.com/youtube/terms
+
+// Helper function to display JavaScript value on HTML page.
+function showResponse(response) {
+    var responseString = JSON.stringify(response, '', 2);
+    document.getElementById('response').innerHTML += responseString;
+}
+
+// Called automatically when JavaScript client library is loaded.
+function onClientLoad() {
+    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
+
+// Called automatically when YouTube API interface is loaded (see line 9).
+function onYouTubeApiLoad() {
+    // This API key is intended for use only in this lesson.
+    // See https://goo.gl/PdPA1 to get a key for your own applications.
+    gapi.client.setApiKey('AIzaSyD_foUsKUlw6i2ErPz7F97fRLeIqrYVDgs');
+    
+    // Add code here to test out showResponse():
+    showResponse("Hooray!");
+}
+
+console.log("Hello World!");
+
